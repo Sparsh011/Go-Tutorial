b8aedb088dc519c3cdce0e287e73aa8163257a73
Sparsh Chadha <93155464+Sparsh011@users.noreply.github.com>
2024-08-13 02:06:34 +0530

    arrays done

---
+++ arrays/go.mod
@@ -0,0 +1,3 @@
+module goarrays
+
+go 1.22.6
---
+++ arrays/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		Note: In Golang, Arrays are not used a lot because of minimal operations that we can perform on them.
+			  Eg. We can't even sort an array, hence we use SLICES most of the time in Go
+	*/
+	var arr = []string{"Hello", "Bye", "Good night"}
+	fmt.Println("Array is : ", arr)
+	arr = append(arr, "20")
+	fmt.Println("Array after appending : ", arr)
+	fmt.Println("Lenght of array: ", len(arr))
+
+	fmt.Println("Traversing array: ")
+
+	for item := range arr {
+		fmt.Println("Item index is : ", item, " and item value is : ", arr[item])
+	}
+}
